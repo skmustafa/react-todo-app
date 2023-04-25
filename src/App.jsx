5fc24d6488a847f7e6827dadafcf447f05477dd2
sk_mustafa <mohammedmustafaparvez@gmail.com>
2023-04-25 20:51:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className='container my-3 mx-3'>
+        <div className='row mt-3 p-5 ms-5'>
+          <h1 className="bg-warning">Hello World</h1>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
